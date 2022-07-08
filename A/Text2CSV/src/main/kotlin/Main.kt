d859cd671c463b45fa6fcdfd0ccf8b3beb420d45
Liam M <liam.m@realtyna.net>
2022-07-08 19:35:10 +0430

    A part - Remove empty lines from text & add text parts to list

--- A/Text2CSV/src/main/kotlin/Main.kt
+++ A/Text2CSV/src/main/kotlin/Main.kt
@@ -6,7 +6,15 @@ import java.nio.file.Paths
 fun main(args: Array<String>) {
     var text = readFileAsString("sample.txt")
     text = text.replace(".DID|.Date|.Cat|[a-z]|\\\\|\\t|\\w+\\d+".toRegex(), "")
+    text = text.replace("(?m)^[ \\t]*\\r?\\n".toRegex(), "*")
 
+    val txtList = text.split("\\*".toRegex())
+    val csvList = mutableListOf<List<String>>()
+    txtList.forEach { textPart ->
+        if (textPart.isNotBlank()) {
+            csvList.add(listOf(textPart))
+        }
+    }
 }
 
 fun readFileAsString(path: String): String {
