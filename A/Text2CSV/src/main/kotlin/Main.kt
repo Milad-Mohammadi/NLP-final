c173a0ce54cb267b191a824bd3534a6cbef44d8b
Milad Mohammadi <Mohammadi.Dev@gmail.com>
2022-07-08 19:51:30 +0430

    Refactored some code and added functions

--- A/Text2CSV/src/main/kotlin/Main.kt
+++ A/Text2CSV/src/main/kotlin/Main.kt
@@ -5,11 +5,21 @@ import java.nio.file.Paths
 
 
 fun main(args: Array<String>) {
-    var text = readFileAsString("sample.txt")
-    text = text.replace(".DID|.Date|.Cat|[a-z]|\\\\|\\t|\\w+\\d+".toRegex(), "")
-    text = text.replace("(?m)^[ \\t]*\\r?\\n".toRegex(), "*")
+    val text = readFileAsString("sample.txt")
+    val txtList = getPreparedTextParts(text)
+    val csvList = getCSVList(txtList)
+    val csvFile = File("./output.csv")
+    csvFile.writeAsCSV(csvList)
+}
 
-    val txtList = text.split("\\*".toRegex())
+fun getPreparedTextParts(text: String): List<String> {
+    var tempText = text
+    tempText = tempText.replace(".DID|.Date|.Cat|[a-z]|\\\\|\\t|\\w+\\d+".toRegex(), "")
+    tempText = tempText.replace("(?m)^[ \\t]*\\r?\\n".toRegex(), "*")
+    return tempText.split("\\*".toRegex())
+}
+
+fun getCSVList(txtList: List<String>): List<List<String>> {
     val csvList = mutableListOf<List<String>>()
     txtList.forEach { textPart ->
         if (textPart.isNotBlank()) {
@@ -17,11 +27,8 @@ fun main(args: Array<String>) {
             csvList.add(sentences)
         }
     }
-
-    val csvFile = File("./output.csv")
-    csvFile.writeAsCSV(csvList)
+    return csvList
 }
-
 fun readFileAsString(path: String): String {
     var result: String? = ""
     try {
