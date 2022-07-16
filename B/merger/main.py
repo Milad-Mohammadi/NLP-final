731336ab6df2b6609e9fcddf9295062b41030e22
Liam M <liam.m@realtyna.net>
2022-07-16 19:06:42 +0430

    Added merger code to merge en and fa datasets

---
+++ B/merger/main.py
@@ -0,0 +1,15 @@
+with open("TEP.en-fa.en", "r") as en:
+    english = en.read().splitlines()
+
+with open("TEP.en-fa.fa", "r") as fa:
+    farsi = fa.read().splitlines()
+
+merged = []
+for index, en_line in enumerate(english):
+    merged_line = en_line + ' -> ' + farsi[index]
+    merged.append(merged_line)
+
+file = open("en_fa.txt", "w")
+for line in merged:
+    file.write(line + '\n')
+file.close()
